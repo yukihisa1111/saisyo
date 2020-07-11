40d1076e000282d84246185700b5f42e08b4dfa3
Yuki <shiruku11111@gmail.com>
2020-07-12 06:25:51 +0900

    unity chan to raycast ireta yo

---
+++ Assets/screpts/Raycaster.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycaster : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButton(0))
+        {
+            //メインカメラ上のマウスカーソルのある位置からRayを飛ばす
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            //Vector3 vec = Input.mousePosition;
+            //Debug.Log(vec);
+
+            //Rayの長さ
+            float maxDistance = 10;
+
+            RaycastHit2D hit = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction, maxDistance);
+
+
+            //なにかと衝突した時だけそのオブジェクトの名前をログに出す
+            if (hit.collider)
+            {
+                Debug.Log(hit.collider.gameObject.name);
+            }
+        }
+    }
+}
